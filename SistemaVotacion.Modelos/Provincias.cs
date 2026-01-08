e990f57b367f246116d5ec66f79f4e16ff65644d
Josue <josuem3693@gmail.com>
2026-01-08 00:07:26 -0500

    Agregar primer modelo de la base de datos

---
+++ SistemaVotacion.Modelos/Provincias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class Provincias
+    {
+        [Key] public int Id { get; set; }
+
+        public string NombreProv { get; set; }
+
+        public int CodigoPostal { get; set; }
+    }
+}

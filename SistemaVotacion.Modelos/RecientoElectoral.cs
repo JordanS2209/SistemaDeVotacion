861c362fa0ced9290f58aaf2568574d74c83d970
Josue <josuem3693@gmail.com>
2026-01-08 19:45:53 -0500

    Modelo RecintoElectoral agregada

---
+++ SistemaVotacion.Modelos/RecientoElectoral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class RecientoElectoral
+    {
+        [Key] public int Id { get; set; }
+
+        public string NombreRecinto { get; set; }
+
+        public string DetalleRecinto { get; set; }
+
+        public string? DireccionRecinto { get; set; }
+
+        public int IdParroquia { get; set; }
+
+        public Parroquia? Parroquia { get; set; }
+    }
+}

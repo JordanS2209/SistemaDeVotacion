dfe039acd36ab3f670009315b45142a4016c8722
Josue <josuem3693@gmail.com>
2026-01-08 20:51:45 -0500

    Modelo Usuario fue agregado

---
+++ SistemaVotacion.Modelos/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class Usuario
+    {
+        [Key] public int Id { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Email { get; set; }
+
+        public int IdRol { get; set; }
+
+        public int IdTipoIdentificacion { get; set; }
+
+        public string NumeroIdentificacion { get; set; }
+
+
+        //public TipoIdentificacion? TipoIdentificacion { get; set; }
+
+        public virtual Rol? Rol { get; set; }
+
+        //public Votante? Votante { get; set; }
+    }
+}

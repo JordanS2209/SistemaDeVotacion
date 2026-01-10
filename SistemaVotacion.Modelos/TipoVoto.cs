68479f1ec85a50e61a4e7758cdd03ef89076e0b2
Josue <josuem3693@gmail.com>
2026-01-09 21:07:38 -0500

    Modelo TipoVoto fue agregado

---
+++ SistemaVotacion.Modelos/TipoVoto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class TipoVoto
+    {
+        [Key] public int IdTipoVoto { get; set; }
+        public string NombreTipo { get; set; } = null!;
+
+        public  List<VotoDetalle>? VotosAsociados { get; set; } = new List<VotoDetalle>();
+    }
+}

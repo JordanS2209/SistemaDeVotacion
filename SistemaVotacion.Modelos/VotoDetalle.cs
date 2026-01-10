f074474720d09bf3c12c53d9e7aa3aea2d04079d
Josue <josuem3693@gmail.com>
2026-01-09 21:05:32 -0500

    Modelo VotoDetalle fue agregado

---
+++ SistemaVotacion.Modelos/VotoDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class VotoDetalle
+    {
+        [Key] public int IdVoto { get; set; }
+        public int IdTipoVoto { get; set; }
+        public int IdJunta { get; set; }
+        public int IdProceso { get; set; }
+
+        //public  TipoVoto? TipoVoto { get; set; }
+
+        public  JuntaReceptora? Junta { get; set; }
+
+        //public  ProcesoElectoral? Proceso { get; set; }
+    }
+}

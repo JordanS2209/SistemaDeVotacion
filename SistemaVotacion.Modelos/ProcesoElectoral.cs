2fe43421e10e84bba69669c2874320a056cedd83
Josue <josuem3693@gmail.com>
2026-01-09 21:21:49 -0500

    Modelo ProcesoElectoral fue agregado

---
+++ SistemaVotacion.Modelos/ProcesoElectoral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class ProcesoElectoral
+    {
+        [Key] public int IdProceso { get; set; }
+        public string NombreProceso { get; set; } = null!;
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int IdTipoProceso { get; set; }
+
+        //public  TipoProceso? TipoProceso { get; set; }
+
+        //public  List<Lista> Listas { get; set; } = new List<Lista>();
+
+        //public  List<Dignidad> Dignidades { get; set; } = new List<Dignidad>();
+
+        //public  List<Padron> RegistroPadron { get; set; } = new List<Padron>();
+    }
+}

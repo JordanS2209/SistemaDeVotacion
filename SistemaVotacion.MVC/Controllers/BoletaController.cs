62f01ffceccb28d1a36e7f8f1a3289a7e6e9c849
DIAZ YAMBERLA JONATHAN STEVEN <jsdiazy@utn.edu.ec>
2026-01-10 06:48:28 -0500

    vista para la boleta

---
+++ SistemaVotacion.MVC/Controllers/BoletaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVotacion.MVC.Controllers
+{
+    public class BoletaController : Controller
+    {
+        // GET: BoletaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BoletaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BoletaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BoletaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BoletaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BoletaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BoletaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BoletaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SistemaVotacion.Modelos/Boleta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotacion.Modelos
+{
+    public class Boleta
+    {
+        [Key]
+        public int IdBoleta { get; set; }
+
+        // Proceso electoral al que pertenece
+        public int IdProcesoElectoral { get; set; }
+        public ProcesoElectoral ProcesoElectoral { get; set; }
+
+        // Dignidad que se vota (Presidente, Alcalde, etc.)
+        public int IdDignidad { get; set; }
+        public Dignidad Dignidad { get; set; }
+
+        // Lista seleccionada
+        public int IdLista { get; set; }
+        public Lista Lista { get; set; }
+
+        // Tipo de voto
+        public int IdTipoVoto { get; set; }
+        public TipoVoto TipoVoto { get; set; }
+
+        // Control
+        public DateTime FechaEmision { get; set; }
+        public bool Emitida { get; set; }
+    }
+}

f4fc1102c10e271208cc60a703c4a89c26457585
Josue <josuem3693@gmail.com>
2026-01-20 19:35:14 -0500

    Se agrego los Controladores de Actas  y de Dignidades al MVC

---
+++ SistemaVotacion.MVC/Controllers/DignidadesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVotacion.MVC.Controllers
+{
+    public class DignidadesController : Controller
+    {
+        // GET: DignidadesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DignidadesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DignidadesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DignidadesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DignidadesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DignidadesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DignidadesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DignidadesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
